49f71fe2fd2981b620581c2224f345ce4cd3bd99
vasilii <porokh.vs@gmail.com>
2022-06-07 00:43:31 +0300

    change webpack

---
+++ webpackConfig/webpackCommon.js
@@ -0,0 +1,48 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack')
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = {
+	entry: './src/index.js',
+	module: {
+		rules: [
+			{ test: /\.txt$/, use: 'raw-loader' },
+			{
+                test: /\.(png|svg|jpg|gif|ttf|bin)$/,
+                use: [{
+                  loader: 'file-loader',
+                  options: {
+                    name: '[name].[ext]',
+                    outputPath: ''
+                  }
+                }]
+            },
+              { test: /\.(obj|glb|gltf|FBX)$/,
+                use: [{
+                  loader: 'file-loader',
+                  options: {
+                    name: '[name].[ext]',
+                    outputPath: ''
+                  }
+                }]
+              },
+            {
+                test: /\.css$/i,
+                use: [
+                    { loader: "style-loader", options: { injectType: "styleTag" } },
+                    'css-loader'
+                ]
+            },
+
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+            template: './public/index.html'
+		}),
+		new webpack.ProvidePlugin({
+			THREE: 'three'
+		})
+	]
+};
